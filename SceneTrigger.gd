44dd1b30f17bd8801392a62ba8900230724a9d15
christieg <minifighterjets@gmail.com>
2024-08-16 16:38:35 -0500

    Successfully updated to godot 4.3

--- SceneTrigger.gd
+++ SceneTrigger.gd
@@ -3,10 +3,8 @@ class_name SceneTrigger extends Area2D
 @export var new_position: Vector2
 var scene_root = "res://"
 
-#signal house_interior_entered()
-
 func _on_body_entered(body):
 	print("something is on the trigger!! " + body.name)
-	#if body.name == "player":
-		#var full_path = scene_root + connected_scene + ".tscn"
-		#SceneLoader.load_scene(full_path)
+	if body.name == "player":
+		# load the next scene
+		pass
