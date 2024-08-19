aa0ac15cef946e3f0b77e53e4e3a6ff39c1639e2
christieg <minifighterjets@gmail.com>
2024-08-19 14:17:51 -0500

    added npc, and raycast to detect when near an npc

--- Entities/NPC/States/NPC_idle.gd
+++ Entities/NPC/States/NPC_idle.gd
@@ -6,20 +6,10 @@ extends State
 func enter() -> void:
 	super()
 	# do the idle animation
-	animations.player_idle_animation()
+	animations.play_idle_animations()
 
 
-func process_input(event: InputEvent) -> State:
-	# on ui_accept, need to see if player raycast is colliding with something
-	var player_raycast = entity.get_node("Raycast2D")
-	if event.is_action_pressed("ui_accept"):
-		if player_raycast.is_colliding():
-			var collider = player_raycast.get_collider()
-			if collider is NPC:
-				print("I collided with an NPC!")
-	#check if player is moving
-	if movement.get_input_direction():
-		return move_state
+func process_input(_event: InputEvent) -> State:
 	return null
 
 
--- Entities/Player/States/idle.gd
+++ Entities/Player/States/idle.gd
@@ -3,6 +3,8 @@ extends State
 @export var move_state: State
 @export var talking_state: State
 
+var player_raycast : RayCast2D
+
 func enter() -> void:
 	super()
 	# do the idle animation
@@ -12,6 +14,14 @@ func enter() -> void:
 func process_input(event: InputEvent) -> State:
 	if movement.get_input_direction():
 		return move_state
+	if event.is_action_pressed("ui_accept"):
+		player_raycast = entity.get_node("Raycast")
+		if player_raycast.is_colliding():
+			var collider = player_raycast.get_collider()
+			print(collider)
+			print("raycast is colliding with something.")
+			if player_raycast.get_collider() is NPC:
+				print("PLAYER COLLIDED WITH NPC!")
 	return null
 
 
